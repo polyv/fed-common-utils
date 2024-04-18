66102a4518c10569cee948d08c836f7f35cb17f7
Mr.Luo <heeroluo@126.com>
2024-04-18 11:19:18 +0800

    构建

--- dist/es/browser.d.ts
+++ dist/es/browser.d.ts
@@ -1,15 +1,20 @@
 /**
- * 判断当前浏览器是否运行在移动设备上（User-Agent 识别为主，特征判断为辅）。
+ * 检测当前浏览器是否运行在移动设备上（User-Agent 识别为主，特征判断为辅）。
  * @returns 当前浏览器是否运行在移动设备上。
  */
 export declare function isMobile(): boolean;
 /**
- * 判断当前浏览器是否为傲游浏览器。
+ * 检测当前浏览器是否具备移动设备特征（特征判断为主，User-Agent 识别为辅）。
+ * @returns 当前浏览器是否具备移动设备特征。
+ */
+export declare function hasMobileFeature(): boolean;
+/**
+ * 检测当前浏览器是否为傲游浏览器。
  * @returns 当前浏览器是否为傲游浏览器。
  */
 export declare function isMaxthon(): any;
 /**
- * 判断当前浏览器是否支持通过 MSE 播放 H264 视频。
+ * 检测当前浏览器是否支持通过 MSE 播放 H264 视频。
  * @returns 当前浏览器是否支持通过 MSE 播放 H264 视频。
  */
 export declare function supportMSEH264(): boolean;
