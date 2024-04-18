2573a96fcd5759bc1fb76c583559a59f62c8d785
Mr.Luo <heeroluo@126.com>
2024-04-18 11:18:26 +0800

    增加 hasMobileFeature 从特性角度检测是否移动设备

--- demo/browser.js
+++ demo/browser.js
@@ -1,9 +1,11 @@
 import {
   isMobile,
+  hasMobileFeature,
   isMaxthon,
   supportMSEH264
 } from '@/browser';
 
 document.getElementById('is-mobile').innerText = '是否移动端：' + isMobile();
+document.getElementById('has-mobile-feature').innerText = '是否有移动端特征：' + hasMobileFeature();
 document.getElementById('is-maxthon').innerText = '是否傲游浏览器：' + isMaxthon();
 document.getElementById('support-mse-h264').innerText = '是否支持 MSE(H264)：' + supportMSEH264();
--- src/browser.ts
+++ src/browser.ts
@@ -1,26 +1,45 @@
 /**
- * 判断当前浏览器是否运行在移动设备上（User-Agent 识别为主，特征判断为辅）。
+ * 检测当前浏览器是否运行在移动设备上（User-Agent 识别为主，特征判断为辅）。
  * @returns 当前浏览器是否运行在移动设备上。
  */
 export function isMobile() {
   const ua = navigator.userAgent;
-  if (
-    /mobile|android/i.test(ua) ||
-    (/\bMacintosh\b/.test(ua) && navigator.maxTouchPoints)
-  ) {
+  if (/mobile|android/i.test(ua)) {
     return true;
-  } else if (
-    /\b(Windows\sNT|Macintosh|x86(_(32|64))?|amd64|i[1-6]86)\b/.test(ua) ||
-    !('onorientationchange' in window)
-  ) {
+  } else if (/\b(Windows\sNT|Macintosh|x86(_(32|64))?|amd64|i[1-6]86)\b/.test(ua)) {
     return false;
   } else {
-    return true;
+    return 'onorientationchange' in window;
   }
 }
 
 /**
- * 判断当前浏览器是否为傲游浏览器。
+ * 检测当前浏览器是否具备移动设备特征（特征判断为主，User-Agent 识别为辅）。
+ * @returns 当前浏览器是否具备移动设备特征。
+ */
+export function hasMobileFeature() {
+  const platform = navigator.platform;
+  if ('onorientationchange' in window) {
+    return true;
+  } else if (/^(iPhone|iPod|iPad)/.test(platform)) {
+    return true;
+  } else if (platform === 'Win32' || /^Linux\s*(x86|i686)/.test(platform)) {
+    return false;
+  } else if (platform === 'MacIntel' || platform === 'Macintosh') {
+    return navigator.maxTouchPoints > 0;
+  } else if (typeof matchMedia !== 'undefined') {
+    // 这个判断在触屏电脑上的结果是 true，存在误判可能性，所以优先级放到最后
+    return matchMedia('(hover: none)').matches &&
+      matchMedia('(pointer: coarse)').matches &&
+      !matchMedia('(pointer: fine)').matches;
+  } else {
+    return false;
+  }
+}
+
+
+/**
+ * 检测当前浏览器是否为傲游浏览器。
  * @returns 当前浏览器是否为傲游浏览器。
  */
 export function isMaxthon() {
@@ -36,7 +55,7 @@ export function isMaxthon() {
 }
 
 /**
- * 判断当前浏览器是否支持通过 MSE 播放 H264 视频。
+ * 检测当前浏览器是否支持通过 MSE 播放 H264 视频。
  * @returns 当前浏览器是否支持通过 MSE 播放 H264 视频。
  */
 export function supportMSEH264() {
